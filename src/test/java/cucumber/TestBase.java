d744ffd4b6fbe4f7ab5247b62f31519e6e541795
ajuliamm <ajuliammelo@gmail.com>
2024-11-04 12:14:43 -0300

    feat: created testBase file and the feature.

---
+++ src/test/java/cucumber/TestBase.java
@@ -0,0 +1,33 @@
+package cucumber;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestBase {
+    protected static WebDriver driver;
+    public static String URL = "https://www.demoblaze.com/index.html";
+
+    public static WebDriver startDriver(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximize");
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
+        return driver;
+    }
+
+    public WebDriver getDriver(){
+        if(driver == null){
+            startDriver();
+        }
+        return driver;
+
+    }
+
+    public void quitDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
